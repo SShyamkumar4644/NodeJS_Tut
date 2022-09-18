37c3054b6ff6ffc221df39665766c8140fdc14ce
SShyamKumar <shyamng48@gmail.com>
2022-09-18 20:34:28 +0530

    Notes added

--- fsmodule.js
+++ fsmodule.js
@@ -1,5 +1,14 @@
 const fs = require('fs');
 
-fs.readFile('file.txt', 'utf8', (err, data) =>{
-    console.log(err, data)
-})
+// fs.readFile('file.txt', 'utf8', (err, data) =>{
+//     console.log(err, data)
+// })
+
+// const a = fs.readFileSync('file.txt')
+// console.log(a.toString())
+
+fs.writeFile('file2.txt', 'This is a data', () =>{
+    console.log('Written to the file')
+});
+
+console.log("Finished reading file")
